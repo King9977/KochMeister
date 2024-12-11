8617971d8b587bb5bb599893d6bcbe3600ad3e6f
Kaya Senay <kaya.senay@pvcomp.ch>
2024-12-11 12:54:14 +0100

    env changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  production: false,
+  production: true,
   supabaseUrl: 'https://lrpiervrpmugozkrztmh.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxycGllcnZycG11Z296a3J6dG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1NjY1MTUsImV4cCI6MjA0OTE0MjUxNX0.eB1ytEAZ0f_BjLbWZYL50mcF4rN9xxywpTf_4Njnpzc',
+  storageBucket: 'recipe-images',
+  applicationId: 'io.ionic.starter'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://lrpiervrpmugozkrztmh.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxycGllcnZycG11Z296a3J6dG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1NjY1MTUsImV4cCI6MjA0OTE0MjUxNX0.eB1ytEAZ0f_BjLbWZYL50mcF4rN9xxywpTf_4Njnpzc',
-  storageBucket: 'recipe-images'
+  storageBucket: 'recipe-images',
+  applicationId: 'io.ionic.starter'
 };
 
 /*
